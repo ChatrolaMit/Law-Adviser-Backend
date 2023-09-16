127bf09a5826e0af5f13ee43a58eb229c02eaf0b
ChatrolaMit <chatrolamit047@gmail.com>
2023-09-17 04:36:31 +0530

    All Advocate API is ready

--- src/features/advocateProfile/model.js
+++ src/features/advocateProfile/model.js
@@ -12,10 +12,26 @@ const addAdvocate = async (obj) =>{
 
 }
 
-const getAllAdvocates = async () =>{
-    const result = await advocate.find({}) 
-    return JSON.stringify(result)
-}
+const getAllAdvocates = async () => {
+    const result = await advocate.find({}).lean();
+  
+    for (let obj of result) {
+      delete obj.hallOfFrames;
+      delete obj.badges;
+      delete obj.description;
+      delete obj.emailId;
+      delete obj.gender;
+      delete obj.publicQuestionReply;
+      delete obj.courts;
+      delete obj.blogs;
+      delete obj.articles;
+      delete obj.videos;
+      delete obj.reviews;
+    }
+  
+    return JSON.stringify(result);
+  };
+  
 
 
 
